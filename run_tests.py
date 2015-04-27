3be4859b0c3e80adf1a149bf7ae1fd995643bcfb
Timothy Cochran <tcochran@tcochran.local>
2015-04-27 08:12:07 -0400

    TC first version of command line visualisation of pyramid

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('nosetests tests/unit/ --xunit-testsuite-name=unit --with-xunit --xunit-file=reports/unit_report.xml')
-os.system('nosetests tests/service/ --xunit-testsuite-name=service --with-xunit --xunit-file=reports/service.xml')
-os.system('nosetests tests/e2e/ --xunit-testsuite-name=e2e --with-xunit --xunit-file=reports/e2e.xml')
+os.system('nosetests tests/unit/ --xunit-testsuite-name=unit_suite --with-xunit --xunit-file=reports/unit_report.xml')
+os.system('nosetests tests/service/ --xunit-testsuite-name=service_suite --with-xunit --xunit-file=reports/service.xml')
+os.system('nosetests tests/e2e/ --xunit-testsuite-name=e2e_suite --with-xunit --xunit-file=reports/e2e.xml')
 
